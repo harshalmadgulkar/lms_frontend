5a0303e5e99d1c4047a0d6b6f567e6aa8de2c274
Harshal <harshal.madgulkar725@gmail.com>
2025-07-15 08:33:59 +0530

    Implement sonner in UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { appStore } from "./app/store.ts";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider store={appStore}>
 			<App />
+			<Toaster />
 		</Provider>
 	</StrictMode>
 );
+
